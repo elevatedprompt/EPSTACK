45d8d99f9aa2a0bfd578b24e6dd53019611fe9ec
ichintu <ichintu@users.noreply.github.com>
2016-06-21 23:11:12 -0700

    accept java license

--- deployEPSTACK.sh
+++ deployEPSTACK.sh
@@ -9,6 +9,7 @@
 ## Install Official Java
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
+echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get -y install oracle-java8-installer
 ## Install elasticsearch
 wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
